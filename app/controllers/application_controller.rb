58be515f16ee8134761a369e695dd00721077a73
Tam Vu <vutam_a@hotmail.com>
2019-11-26 20:46:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
     @team = Team.new(params[:team])
     
     params[:team][:members].each do |member|
-      Member.new(args)
+      Member.new(arg)
     end
     
     @members = Members.all
